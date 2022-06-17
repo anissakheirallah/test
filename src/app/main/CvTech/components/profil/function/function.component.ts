738f9cce85cc5046691cc4b6685f2387d4d031ba
hiba <hiba.allache@gmail.com>
2022-06-17 09:14:39 +0100

    function and skills

--- src/app/main/CvTech/components/profil/function/function.component.ts
+++ src/app/main/CvTech/components/profil/function/function.component.ts
@@ -1,85 +1,286 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Function } from 'app/main/CvTech/models/function';
-import { FunctionService } from 'app/main/CvTech/services/function.service';
+import { HttpErrorResponse } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Function } from "app/main/CvTech/models/function";
+import { FunctionService } from "app/main/CvTech/services/function.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-function',
-  templateUrl: './function.component.html',
-  styleUrls: ['./function.component.scss']
+  selector: "app-function",
+  templateUrl: "./function.component.html",
+  styleUrls: ["./function.component.scss"],
 })
 export class FunctionComponent implements OnInit {
-  data?: Function[]
-  public func : Function = {name: '', description :''}
-  contentHeader: { headerTitle: string; actionButton: boolean; breadcrumb: { type: string; links: ({ name: string; isLink: boolean; link: string; } | { name: string; isLink: boolean; link?: undefined; })[]; }; };
-  constructor(private functionService : FunctionService) { }
+  public data?: Function[];
+  public func: Function = { id: null, name: "", description: "" };
+  public pagePosition = 1;
+  public totalPages = 0;
+  contentHeader: {
+    headerTitle: string;
+    actionButton: boolean;
+    breadcrumb: {
+      type: string;
+      links: (
+        | { name: string; isLink: boolean; link: string }
+        | { name: string; isLink: boolean; link?: undefined }
+      )[];
+    };
+  };
 
-  funcForm = new FormGroup({
-    name : new FormControl(''),
-    description : new FormControl('')
-  })
+  submitted = false;
+
+  public chkBoxSelected = [];
+
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private functionService: FunctionService
+  ) {}
+
+  public funcForm: FormGroup = new FormGroup({
+    name: new FormControl(""),
+    description: new FormControl(""),
+  });
 
   ngOnInit(): void {
-    this.getData()
+    this.getData();
     this.contentHeader = {
-      headerTitle: 'Function',
+      headerTitle: "Function",
       actionButton: true,
       breadcrumb: {
-        type: '',
+        type: "",
         links: [
           {
-            name: 'Home',
+            name: "Home",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'CvTech',
+            name: "CvTech",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'Profil',
+            name: "Profil",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'Function',
-            isLink: false
-          }
-        ]
-      }
+            name: "Function",
+            isLink: false,
+          },
+        ],
+      },
     };
+    this.funcForm = this.formBuilder.group({
+      name: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern("[a-zA-Z ]*"),
+        ],
+      ],
+      description: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(45),
+        ],
+      ],
+    });
   }
 
-  getData() : void 
-  {
-      this.functionService.getFunctions().subscribe(
-        (response : any) => {this.data = response.content, console.log(response)}
-      )
+  modalOpenPrimary(modalPrimary) {
+    this.modalService.open(modalPrimary, {
+      centered: true,
+      windowClass: "modal modal-primary",
+    });
   }
 
-  addData() : void 
-  {
-    this.func = this.funcForm.value
-    const functionData = 
-    {
-      name : this.func.name,
-      description : this.func.description
+  page = 1;
+  count = 0;
+  name = "";
+  description = "";
+
+  public pageChanged(event: any): void {
+    this.page = event;
+    console.log(event);
+    this.getData();
+  }
+
+  getParams(page: number, pageSize: number, name: string, description: string) {
+    let params: any = {};
+    if (page) {
+      params["page"] = page - 1;
     }
-    this.functionService.addFunction(functionData).subscribe(
-      (response : Function) => { console.log(response), window.location.reload()},
-      (error : HttpErrorResponse) =>  { alert(error.message)}
-    )
+    if (pageSize) {
+      params["size"] = pageSize;
+    }
+    if (name) {
+      params["name"] = name;
+    }
+    if (description) {
+      params["description"] = description;
+    }
+
+    return params;
   }
 
-  deleteData(id : number)
-  {
-    this.functionService.deleteFunction(id).subscribe(
-      () => { window.location.reload() },
-      (error : HttpErrorResponse) => {  alert(error.message)}
-    )
-    
+  // getData(): void {
+  //   this.functionService.getFunctions().subscribe((response: any) => {
+  //     (this.data = response.content), console.log(response);
+  //   });
+  // }
+
+  getData(): void {
+    const params = {
+      page: this.page - 1,
+      size: 4,
+      name: this.name,
+      description: this.description,
+    };
+
+    this.functionService.getAllPagination(params).subscribe({
+      next: (response: any) => {
+        const { content, totalElements, totalPages } = response;
+        this.count = totalElements;
+        this.totalPages = totalPages * 10;
+        this.data = response.content;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 
+  private modal = null;
+  private id = 0;
+
+  modalOpenDanger(modalDanger, id: any) {
+    this.id = id;
+    this.modal = this.modalService.open(modalDanger, {
+      centered: true,
+      windowClass: "modal modal-danger",
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.funcForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.funcForm.invalid) {
+      return;
+    }
+    this.func = this.funcForm.value;
+
+    this.addData();
+  }
+
+  // addData(): void {
+  //   this.func = this.funcForm.value;
+  //   const functionData = {
+  //     name: this.func.name,
+  //     description: this.func.description,
+  //   };
+  //   this.functionService.addFunction(functionData).subscribe(
+  //     (response: Function) => {
+  //       console.log(response), window.location.reload();
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   );
+  // }
+
+  public addData(): void {
+    const functionData = {
+      name: this.func.name,
+      description: this.func.description,
+    };
+    this.functionService.addFunction(functionData).subscribe({
+      next: (data) => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.error(err);
+        alert(err.message);
+      },
+    });
+  }
+
+  // deleteData(id: number) {
+  //   this.functionService.deleteFunction(id).subscribe(
+  //     () => {
+  //       window.location.reload();
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   );
+  // }
+
+  // ------------ Edit function ------------
+
+  modalEdit(modalPrimaryedit, id) {
+    this.functionService.getFunction(id).subscribe({
+      next: (data) => {
+        this.func = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+    this.modalService.open(modalPrimaryedit, {
+      centered: true,
+      windowClass: "modal modal-primary",
+    });
+  }
+
+  updateSkill(): void {
+    const updateF = {
+      id: this.func.id,
+      name: this.func.name,
+      description: this.func.description,
+    };
+    this.functionService.updateFunction(updateF.id, updateF).subscribe({
+      next: (data) => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  // ------------- delete -------------- //
+
+  public deleteData() {
+    console.log(this.id);
+
+    this.modal.close("Accept click");
+    this.functionService.deleteFunction(this.id).subscribe({
+      next: () => {
+        console.log("Function deleted !", this.id);
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.funcForm.reset();
+  }
 }
--- src/app/main/CvTech/components/profil/skills/skills.component.ts
+++ src/app/main/CvTech/components/profil/skills/skills.component.ts
@@ -1,101 +1,287 @@
-import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component, NgModule, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { Skill } from '../../../models/skill';
-import { SkillService } from '../../../services/skill.service';
-
+import { HttpErrorResponse, HttpResponse } from "@angular/common/http";
+import { Component, NgModule, OnInit } from "@angular/core";
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Skill } from "../../../models/skill";
+import { SkillService } from "../../../services/skill.service";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  selector: "app-skills",
+  templateUrl: "./skills.component.html",
+  styleUrls: ["./skills.component.scss"],
 })
+export class SkillsComponent implements OnInit {
+  // public skill: Skill = { id: null, name: undefined, description: undefined };
 
+  public skill: Skill = {
+    id: null,
+    name: "",
+    description: "",
+  };
 
+  // public contentHeader: object;
+  public pagePosition = 1;
+  public totalPages = 0;
+  contentHeader: {
+    headerTitle: string;
+    actionButton: boolean;
+    breadcrumb: {
+      type: string;
+      links: (
+        | { name: string; isLink: boolean; link: string }
+        | { name: string; isLink: boolean; link?: undefined }
+      )[];
+    };
+  };
 
-export class SkillsComponent implements OnInit 
-{
-  public skill : Skill = {name: '', description :''}
-  public contentHeader: object;
   public data?: Skill[];
 
-  constructor(private skillService : SkillService) { }
+  submitted = false;
 
-  skillForm = new FormGroup({
-    name: new FormControl(''),
-    description: new FormControl('')
-  })
+  public chkBoxSelected = [];
 
-  ngOnInit(): void 
-  {
-    this.getData()
-    
+  constructor(
+    private modalService: NgbModal,
+    private formBuilder: FormBuilder,
+    private skillService: SkillService
+  ) {}
+
+  public skillForm: FormGroup = new FormGroup({
+    name: new FormControl(""),
+    description: new FormControl(""),
+  });
+
+  ngOnInit(): void {
+    this.getData();
+
     this.contentHeader = {
-      headerTitle: 'Skills',
+      headerTitle: "Skills",
       actionButton: true,
       breadcrumb: {
-        type: '',
+        type: "",
         links: [
           {
-            name: 'Home',
+            name: "Home",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'CvTech',
+            name: "CvTech",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'Profil',
+            name: "Profil",
             isLink: true,
-            link: '/'
+            link: "/",
           },
           {
-            name: 'Skills',
-            isLink: false
-          }
-        ]
-      }
+            name: "Skills",
+            isLink: false,
+          },
+        ],
+      },
     };
+
+    this.skillForm = this.formBuilder.group({
+      name: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.pattern("[a-zA-Z ]*"),
+        ],
+      ],
+      description: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(45),
+        ],
+      ],
+    });
   }
 
-  public getData() : void
-  {
-    this.skillService.getSkills().subscribe(
-      (res : any) => {
-        this.data = res.content
-        //console.log(res)
-      } ,
-      (error : HttpErrorResponse) =>
-      {
-        alert(error.message)
-      }
-    )
+  modalOpenPrimary(modalPrimary) {
+    this.modalService.open(modalPrimary, {
+      centered: true,
+      windowClass: "modal modal-primary",
+    });
   }
 
-  public addData() : void 
-  {
-    this.skill = this.skillForm.value;
-   
-    const skillData = 
-    {
-      name : this.skill.name,
-      description : this.skill.description
+  page = 1;
+  count = 0;
+  name = "";
+  description = "";
+
+  public pageChanged(event: any): void {
+    this.page = event;
+    console.log(event);
+    this.getData();
+  }
+
+  getParams(page: number, pageSize: number, name: string, description: string) {
+    let params: any = {};
+    if (page) {
+      params["page"] = page - 1;
     }
-    this.skillService.addSkill(skillData).subscribe(
-      (response : Skill) => { console.log(response), window.location.reload() },
-      (error : HttpErrorResponse) => { alert(error.message) }
-      )
-    
+    if (pageSize) {
+      params["size"] = pageSize;
+    }
+    if (name) {
+      params["name"] = name;
+    }
+    if (description) {
+      params["description"] = description;
+    }
+
+    return params;
   }
 
-  public deleteData(id : number) : void 
-  {
-    this.skillService.deleteSkill(id).subscribe(
-      () => { window.location.reload() },
-      (error : HttpErrorResponse) => {  alert(error.message)}
-      )
+  getData(): void {
+    const params = {
+      page: this.page - 1,
+      size: 4,
+      name: this.name,
+      description: this.description,
+    };
+
+    this.skillService.getAllPagination(params).subscribe({
+      next: (response: any) => {
+        const { content, totalElements, totalPages } = response;
+        this.count = totalElements;
+        this.totalPages = totalPages * 10;
+        this.data = response.content;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
   }
 
+  private modal = null;
+  private id = 0;
+
+  modalOpenDanger(modalDanger, id: any) {
+    this.id = id;
+    this.modal = this.modalService.open(modalDanger, {
+      centered: true,
+      windowClass: "modal modal-danger",
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.skillForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.skillForm.invalid) {
+      return;
+    }
+    this.skill = this.skillForm.value;
+
+    this.addData();
+  }
+
+  // public getData(): void {
+  //   this.skillService.getSkills().subscribe(
+  //     (res: any) => {
+  //       this.data = res.content;
+  //       //console.log(res)
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   );
+  // }
+
+  public addData(): void {
+    const skillData = {
+      name: this.skill.name,
+      description: this.skill.description,
+    };
+    this.skillService.addSkill(skillData).subscribe({
+      next: (data) => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.error(err);
+        alert(err.message);
+      },
+    });
+  }
+
+  // public deleteData(id: number): void {
+  //   this.skillService.deleteSkill(id).subscribe(
+  //     () => {
+  //       window.location.reload();
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       alert(error.message);
+  //     }
+  //   );
+  // }
+
+  // ------------ Edit skill ------------
+
+  modalEdit(modalPrimaryedit, id) {
+    this.skillService.getSkill(id).subscribe({
+      next: (data) => {
+        this.skill = data;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+    this.modalService.open(modalPrimaryedit, {
+      centered: true,
+      windowClass: "modal modal-primary",
+    });
+  }
+
+  updateSkill(): void {
+    const updateS = {
+      id: this.skill.id,
+      name: this.skill.name,
+      description: this.skill.description,
+    };
+    this.skillService.updateSkill(updateS.id, updateS).subscribe({
+      next: (data) => {
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  // ------------- delete -------------- //
+
+  public deleteData() {
+    console.log(this.id);
+
+    this.modal.close("Accept click");
+    this.skillService.deleteSkill(this.id).subscribe({
+      next: () => {
+        console.log("Skill deleted !", this.id);
+        this.ngOnInit();
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.skillForm.reset();
+  }
 }
