9359f281b5f706ff6b04082a40203f1ce36f4c20
ghizlane <g.elkarroumi>
2022-08-26 17:45:36 +0100

    project structure

--- src/app/main/company/components/company/add-company/add-company.component.ts
+++ src/app/main/company/components/company/add-company/add-company.component.ts
@@ -84,7 +84,6 @@ export class AddCompanyComponent implements OnInit {
           [
             Validators.required,
             Validators.pattern(/^[\+|0{1,2}]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/),
-
           ]
         ],
         address: ['', Validators.required]
